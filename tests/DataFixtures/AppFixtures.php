d7f236c62a5e588bf7cc1dcb24b37b6b875dd8e6
Rindra <rahrindra@gmail.com>
2025-03-24 11:25:39 +0100

    Ajout des tests fonctionnels et utilisation des fixtures

---
+++ tests/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\DataFixtures;
+
+use App\Domain\Model\Category;
+use App\Domain\Model\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $category = new Category();
+        $category->setName('Category 1');
+        $category->setReference('CAT0001');
+        $manager->persist($category);
+
+        // create 20 products! Bam!
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product();
+            $product->setName('product ' . $i + 1);
+            $product->setPrice(mt_rand(10, 100));
+            $product->setQuantity(mt_rand(1, 10));
+            $product->setCategory($category);
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
